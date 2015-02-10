8dd9b68a5f178f402212cc68861718c1bd217472
Stephen Settle <ssettle3@gmail.com>
2015-02-10 14:17:26 -0500

    Basic Tests, HTML, CSS Layout Complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('I am ready to go!')
+var ToDo= function(args){
+
+	this.status = 'open';
+	this.name = name;
+	this.checkOff = function(){
+		this.status = 'closed';
+	};
+	this.unCheckOff = function(){
+		this.status = 'open';
+	};
+	this.deleteItem = function(){
+		this.status = 'deleted';
+	}; 
+
+};
+
+var todos = [];
+
--- test/spec/test.js
+++ test/spec/test.js
@@ -3,11 +3,48 @@
 (function () {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
+  describe('About My To-Do List', function () {
 
+  	beforeEach(function(){
+  		window.item = new ToDo ();
+  	});
+    
+    describe('Ability to Create Items', function () {
+      
+      it('Should create instanceof To-Do', function () {
+      	expect(item instanceof ToDo).to.equal(true);
+      });
+      
+      it('Should be listed as open', function(){
+      	expect(item.status).to.be.equal('open');
+      });
+
+      it ('Should have a name', function(){
+      	expect(item.name).to.be.equal(item.name);
+      	item.name = 'setName';
+      	expect(item.name).to.be.equal('setName');
       });
     });
+
+    describe('Ability to Check Complete Items', function (){
+    	it('Should be able to get completed', function(){
+    		item.checkOff();
+    		expect(item.status).to.be.equal('closed');
+    	});
+
+    	it('Should be able to Un-Check Items', function(){
+    		item.unCheckOff();
+    		expect(item.status).to.be.equal('open');
+    	});    	
+    });
+
+    describe('Ability to Delete Items', function (){
+    	it('Should be able to be deleted', function (){
+    		item.deleteItem();
+    		expect(item.status).to.be.equal('deleted');
+    	});
+    });
+
   });
+
 })();
