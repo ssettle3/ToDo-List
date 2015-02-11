0b3c83b663a3d03e723e4c5e7af1cb8f8d4b9f52
Stephen Settle <ssettle3@gmail.com>
2015-02-10 22:02:13 -0500

    Ability to Add Items to List

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,5 @@
-var ToDo= function(args){
+// Constructor Function
+var ToDo = function(args){
 
 	this.status = 'open';
 	this.name = name;
@@ -14,5 +15,31 @@ var ToDo= function(args){
 
 };
 
+// Array List of todos to push to
 var todos = [];
+
+// Declare Variables
+var list = $('#list-items');
+
+// Target Input Element and Add Item to todos array
+$('#todo-form').on('submit', function(e){
+	e.preventDefault();
+	var item = new ToDo();
+ 	item.name = $('#submit').val();
+	list.append('<li id=a' + todos.length + '>' + item.name + '</li');
+	list.append('<span id=b' + todos.length + '>' + 'X' + '</span>')
+	todos.push(item);
+	this.reset();
+
+
+});
+
+// Toggle Click Complete Item
+$('#list-items').on('click', 'li', function(){
+	$(this).toggleClass('compl');
+
+});
+
+// Delete item from To Do List
+
 
